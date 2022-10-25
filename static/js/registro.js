1fe6689a114ab824143b51333814eae4ff1c8ba6
polo123456789 <pabsan02@gmail.com>
2022-10-25 11:34:59 -0600

    (+) Seleccion de avatar

--- static/js/registro.js
+++ static/js/registro.js
@@ -2,3 +2,25 @@ applyTextValidator("#nombres", validateName);
 applyTextValidator("#apellidos", validateName);
 applyTextValidator("#contraseña", validatePassword);
 applyDateValidation("#nac");
+
+/** @type HTMLInputElement */
+const avatar = document.querySelector("#avatar")
+
+const avatars = [
+    document.querySelector("#avatar1"),
+    document.querySelector("#avatar3"),
+]
+
+const selectAvatar = (selector) => {
+    const avatarId = selector == "#avatar1" ? 1 :
+                     selector == "#avatar3" ? 3 :
+                     1;
+
+    avatar.value = avatarId;
+    avatars.forEach((a) => a.classList.remove("selected"));
+    document.querySelector(selector).classList.add("selected")
+}
+
+avatars.forEach((a) => a.addEventListener("click", () => {
+    selectAvatar(`#${a.id}`)
+}))
