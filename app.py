ede99f6e0aec8dc1c30d508cfc034dd4f4fb43fd
Yasmin1911 <78658537+Yasmin1911@users.noreply.github.com>
2022-10-24 22:26:53 -0600

    Corrección de errores y actualización

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template 
 
 app = Flask(__name__)
 
@@ -14,6 +14,28 @@ def Trivia():
 @app.route('/registro')
 def Registro():
     return render_template("Registro.html")
+
+@app.route('/login')
+def Login():
+    return render_template("Login.html")
+
+@app.route('/perfil')
+def Perfil():
+    DATETIME_STRING_FORMAT = "%d/%m/%Y %H:%M:%S"
+    user = {
+            "nombres": "Usuario",
+            "apellidos": "anonimo",
+            "correo": "email@example.com",
+            "idUser":  12345,
+            "puntuacion":  0,
+            "fechaNacimiento": "2002-11-09",
+            "intentosFallidos": 0,
+            "nivel": 0,
+            "avatar":  "/static/img/avatars/standar.gif"}
+    return render_template("Perfil.html", usuario=user)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug = True, port= 8000)
